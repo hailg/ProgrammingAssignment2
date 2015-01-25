86beff75cefc219d77377da5cf4f4f5bc6253bf2
Le Gia Hai <hailegia@gmail.com>
2015-01-25 11:38:47 +0800

    Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object that stores a matrix 
+## and cache its inversion
 
-## Write a short comment describing this function
+## makeCacheMatrix return a list of following functions:
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the inversion matrix 
+## 4. get the inversion matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversion.matrix <- NULL
+  set <- function(new.x) {
+    x <<- new.x
+    inversion.matrix <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  set.inversion <- function(new.inversion.matrix) {
+    inversion.matrix <<- new.inversion.matrix
+  }
+  
+  get.inversion <- function() {
+    inversion.matrix
+  }
+  
+  list(set = set, get = get, set.inversion = set.inversion, get.inversion = get.inversion)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculate the inversion matrix of the speical input "matrix" x created 
+## with the above function. However, it first checks if the inversion matrix is already 
+## cached. If so, it gets the value from the cache and return it,
+## Otherwise, it calculates and store the inversion matrix into the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inversion.matrix <- x$get.inversion()
+  if (is.null(inversion.matrix)) {
+    message('Calculate inversion for the first time.')
+    data.matrix <- x$get()
+    inversion.matrix <- solve(data.matrix)
+    x$set.inversion(inversion.matrix)
+  }
+  inversion.matrix
 }
